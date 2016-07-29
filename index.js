c7f84ad1dc9be992f6264c9d99df03bc6935a19a
David R. Poindexter III <drpoindexter@gmail.com>
2016-07-29 17:22:16 -0400

    Add a better hello world

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello betty!\n'));
+app.get('/', (req,res) => res.send('Hello Docker, Node, Express World!\n'));
 
 app.listen(PORT);
 console.log(`Running on http://localhost:${PORT}`);
